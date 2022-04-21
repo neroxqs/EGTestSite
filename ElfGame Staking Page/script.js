1b56e1610412f53a656a055edc21e2ff6165ad3c
neroxqs <99805602+neroxqs@users.noreply.github.com>
2022-04-21 18:10:18 +0300

    a

--- ElfGame Staking Page/script.js
+++ ElfGame Staking Page/script.js
@@ -180,7 +180,7 @@ async function loadStakedNFTs() {
     stakedElfsJSONArray = new Array();
     stakedOrcsJSONArray = new Array();
 
-    allStakedIDs.forEach(async function(id){
+    await allStakedIDs.forEach(async function(id){
         options = {
             address: json.mintContractAddress,
             token_id: "" + id,
