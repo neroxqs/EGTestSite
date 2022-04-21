84e92c8315ae7fee5b9a64b4d56a82e281209f24
neroxqs <99805602+neroxqs@users.noreply.github.com>
2022-04-21 16:48:39 +0300

    a

--- ElfGame Staking Page/script.js
+++ ElfGame Staking Page/script.js
@@ -59,9 +59,9 @@ async function loadContracts() {
 
 async function loadNFT() {
     const options = {
-        chain: chainName,
-        token_address: json.mintContractABI+""
-      };
+        chain: "mumbai",
+        token_address: "0x644A21382a8971D7a56073C459e72F08AC6f17C3"
+    };
 
     const NFTs = await Moralis.Web3API.account.getNFTsForContract(options);
 
